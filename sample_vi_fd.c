97f8bbd36decd6ee6162192351ce8c71c6555cc8
simone <79666023+simonegalizzi@users.noreply.github.com>
2024-04-01 15:54:15 +0200

    Add files via upload

---
+++ sample_vi_fd.c
@@ -0,0 +1,378 @@
+#define LOG_TAG "SampleFD"
+#define LOG_LEVEL LOG_LEVEL_INFO
+
+#include "middleware_utils.h"
+#include "sample_utils.h"
+#include "vi_vo_utils.h"
+
+#include <core/utils/vpss_helper.h>
+#include <cvi_comm.h>
+#include <rtsp.h>
+#include <sample_comm.h>
+#include "cvi_tdl.h"
+
+#include <pthread.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <inttypes.h>
+#include <fcntl.h>
+#include <sys/ioctl.h>
+
+#include "ssd1306.h"
+#include "ssd1306.c"
+
+
+static volatile bool bExit = false;
+
+static cvtdl_face_t g_stFaceMeta = {0};
+int status = 0;
+static uint32_t g_size = 0;
+
+MUTEXAUTOLOCK_INIT(ResultMutex);
+
+typedef struct {
+  SAMPLE_TDL_MW_CONTEXT *pstMWContext;
+  cvitdl_service_handle_t stServiceHandle;
+} SAMPLE_TDL_VENC_THREAD_ARG_S;
+
+char* itoa (unsigned long long  value,  char str[],  int radix)
+{
+    char        buf [66];
+    char*       dest = buf + sizeof(buf);
+    bool     sign = false;
+
+    if (value == 0) {
+        memcpy (str, "0", 2);
+        return str;
+    }
+
+    if (radix < 0) {
+        radix = -radix;
+        if ( (long long) value < 0) {
+            value = -value;
+            sign = true;
+        }
+    }
+
+    *--dest = '\0';
+
+    switch (radix)
+    {
+    case 16:
+        while (value) {
+            * --dest = '0' + (value & 0xF);
+            if (*dest > '9') *dest += 'A' - '9' - 1;
+            value >>= 4;
+        }
+        break;
+    case 10:
+        while (value) {
+            *--dest = '0' + (value % 10);
+            value /= 10;
+        }
+        break;
+
+    case 8:
+        while (value) {
+            *--dest = '0' + (value & 7);
+            value >>= 3;
+        }
+        break;
+
+    case 2:
+        while (value) {
+            *--dest = '0' + (value & 1);
+            value >>= 1;
+        }
+        break;
+
+    default:            // The slow version, but universal
+        while (value) {
+            *--dest = '0' + (value % radix);
+            if (*dest > '9') *dest += 'A' - '9' - 1;
+            value /= radix;
+        }
+        break;
+    }
+
+    if (sign) *--dest = '-';
+
+    memcpy (str, dest, buf +sizeof(buf) - dest);
+    return str;
+}
+
+void *run_venc(void *args) {
+  printf("Enter encoder thread\n");
+  SAMPLE_TDL_VENC_THREAD_ARG_S *pstArgs = (SAMPLE_TDL_VENC_THREAD_ARG_S *)args;
+  VIDEO_FRAME_INFO_S stFrame;
+  CVI_S32 s32Ret;
+  cvtdl_face_t stFaceMeta = {0};
+
+  while (bExit == false) {
+    s32Ret = CVI_VPSS_GetChnFrame(0, 0, &stFrame, 2000);
+    if (s32Ret != CVI_SUCCESS) {
+      printf("CVI_VPSS_GetChnFrame chn0 failed with %#x\n", s32Ret);
+      break;
+    }
+
+    {
+      MutexAutoLock(ResultMutex, lock);
+      CVI_TDL_CopyFaceMeta(&g_stFaceMeta, &stFaceMeta);
+    }
+
+    s32Ret = CVI_TDL_Service_FaceDrawRect(pstArgs->stServiceHandle, &stFaceMeta, &stFrame, false,
+                                          CVI_TDL_Service_GetDefaultBrush());
+    if (s32Ret != CVI_TDL_SUCCESS) {
+      CVI_VPSS_ReleaseChnFrame(0, 0, &stFrame);
+      printf("Draw fame fail!, ret=%x\n", s32Ret);
+      goto error;
+    }
+
+    s32Ret = SAMPLE_TDL_Send_Frame_RTSP(&stFrame, pstArgs->pstMWContext);
+    if (s32Ret != CVI_SUCCESS) {
+      CVI_VPSS_ReleaseChnFrame(0, 0, &stFrame);
+      printf("Send Output Frame NG, ret=%x\n", s32Ret);
+      goto error;
+    }
+
+  error:
+    CVI_TDL_Free(&stFaceMeta);
+    CVI_VPSS_ReleaseChnFrame(0, 0, &stFrame);
+    if (s32Ret != CVI_SUCCESS) {
+      bExit = true;
+    }
+  }
+  printf("Exit encoder thread\n");
+  pthread_exit(NULL);
+}
+
+void *run_tdl_thread(void *pHandle) {
+  
+  printf("Enter TDL thread\n");
+  cvitdl_handle_t pstTDLHandle = (cvitdl_handle_t)pHandle;
+
+  VIDEO_FRAME_INFO_S stFrame;
+  cvtdl_face_t stFaceMeta = {0};
+
+  CVI_S32 s32Ret;
+  while (bExit == false) {
+    char msg[200] = "NUM VOLTI ";
+    char buff[3];
+    s32Ret = CVI_VPSS_GetChnFrame(0, VPSS_CHN1, &stFrame, 2000);
+
+    if (s32Ret != CVI_SUCCESS) {
+      printf("CVI_VPSS_GetChnFrame failed with %#x\n", s32Ret);
+      goto get_frame_failed;
+    }
+
+    s32Ret = CVI_TDL_ScrFDFace(pstTDLHandle, &stFrame, &stFaceMeta);
+    if (s32Ret != CVI_TDL_SUCCESS) {
+      printf("inference failed!, ret=%x\n", s32Ret);
+      goto inf_error;
+    }
+
+    if (stFaceMeta.size != g_size) {
+      printf("face count: %d\n", stFaceMeta.size);
+      g_size = stFaceMeta.size;
+      if (g_size>0){
+      status = system(" echo 1 > /sys/class/gpio/gpio440/value");
+      itoa(g_size,buff,4);
+      strcat(msg,buff);
+      //strncpy(msg, buff, sizeof(msg));
+      ssd1306_oled_write_string(1, msg);
+      }else{
+	  status = system(" echo 0 > /sys/class/gpio/gpio440/value");
+	  ssd1306_oled_clear_screen();
+	  }  
+    }
+
+    {
+      MutexAutoLock(ResultMutex, lock);
+      CVI_TDL_CopyFaceMeta(&stFaceMeta, &g_stFaceMeta);
+    }
+
+  inf_error:
+    CVI_VPSS_ReleaseChnFrame(0, 1, &stFrame);
+  get_frame_failed:
+    CVI_TDL_Free(&stFaceMeta);
+    if (s32Ret != CVI_SUCCESS) {
+      bExit = true;
+    }
+  }
+
+  printf("Exit TDL thread\n");
+  pthread_exit(NULL);
+}
+
+static void SampleHandleSig(CVI_S32 signo) {
+  signal(SIGINT, SIG_IGN);
+  signal(SIGTERM, SIG_IGN);
+  printf("handle signal, signo: %d\n", signo);
+  if (SIGINT == signo || SIGTERM == signo) {
+    bExit = true;
+  }
+}
+
+int main(int argc, char *argv[]) {
+  uint8_t rc = 0;
+    
+    // open the I2C device node
+    rc = ssd1306_init(0);
+    rc += ssd1306_oled_default_config(32, 128);
+    if (rc != 0)
+    {
+        printf("no oled attached to /dev/i2c");
+        return 1;
+    }
+    
+    
+  status=system("echo 440 > /sys/class/gpio/export");
+  status=system("echo out > /sys/class/gpio/gpio440/direction");
+  if (argc != 2) {
+    printf(
+        "\nUsage: %s RETINA_MODEL_PATH.\n\n"
+        "\tRETINA_MODEL_PATH, path to retinaface model.\n",
+        argv[0]);
+    return CVI_TDL_FAILURE;
+  }
+
+  signal(SIGINT, SampleHandleSig);
+  signal(SIGTERM, SampleHandleSig);
+
+  SAMPLE_TDL_MW_CONFIG_S stMWConfig = {0};
+
+  CVI_S32 s32Ret = SAMPLE_TDL_Get_VI_Config(&stMWConfig.stViConfig);
+  if (s32Ret != CVI_SUCCESS || stMWConfig.stViConfig.s32WorkingViNum <= 0) {
+    printf("Failed to get senor infomation from ini file (/mnt/data/sensor_cfg.ini).\n");
+    return -1;
+  }
+
+  // Get VI size
+  PIC_SIZE_E enPicSize;
+  s32Ret = SAMPLE_COMM_VI_GetSizeBySensor(stMWConfig.stViConfig.astViInfo[0].stSnsInfo.enSnsType,
+                                          &enPicSize);
+  if (s32Ret != CVI_SUCCESS) {
+    printf("Cannot get senor size\n");
+    return -1;
+  }
+
+  SIZE_S stSensorSize;
+  s32Ret = SAMPLE_COMM_SYS_GetPicSize(enPicSize, &stSensorSize);
+  if (s32Ret != CVI_SUCCESS) {
+    printf("Cannot get senor size\n");
+    return -1;
+  }
+
+  // Setup frame size of video encoder to 1080p
+  SIZE_S stVencSize = {
+      .u32Width = 1280,
+      .u32Height = 720,
+  };
+
+  stMWConfig.stVBPoolConfig.u32VBPoolCount = 3;
+
+  // VBPool 0 for VPSS Grp0 Chn0
+  stMWConfig.stVBPoolConfig.astVBPoolSetup[0].enFormat = VI_PIXEL_FORMAT;
+  stMWConfig.stVBPoolConfig.astVBPoolSetup[0].u32BlkCount = 2;
+  stMWConfig.stVBPoolConfig.astVBPoolSetup[0].u32Height = stSensorSize.u32Height;
+  stMWConfig.stVBPoolConfig.astVBPoolSetup[0].u32Width = stSensorSize.u32Width;
+  stMWConfig.stVBPoolConfig.astVBPoolSetup[0].bBind = true;
+  stMWConfig.stVBPoolConfig.astVBPoolSetup[0].u32VpssChnBinding = VPSS_CHN0;
+  stMWConfig.stVBPoolConfig.astVBPoolSetup[0].u32VpssGrpBinding = (VPSS_GRP)0;
+
+  // VBPool 1 for VPSS Grp0 Chn1
+  stMWConfig.stVBPoolConfig.astVBPoolSetup[1].enFormat = VI_PIXEL_FORMAT;
+  stMWConfig.stVBPoolConfig.astVBPoolSetup[1].u32BlkCount = 2;
+  stMWConfig.stVBPoolConfig.astVBPoolSetup[1].u32Height = stVencSize.u32Height;
+  stMWConfig.stVBPoolConfig.astVBPoolSetup[1].u32Width = stVencSize.u32Width;
+  stMWConfig.stVBPoolConfig.astVBPoolSetup[1].bBind = true;
+  stMWConfig.stVBPoolConfig.astVBPoolSetup[1].u32VpssChnBinding = VPSS_CHN1;
+  stMWConfig.stVBPoolConfig.astVBPoolSetup[1].u32VpssGrpBinding = (VPSS_GRP)0;
+
+  // VBPool 2 for TDL preprocessing
+  stMWConfig.stVBPoolConfig.astVBPoolSetup[2].enFormat = PIXEL_FORMAT_BGR_888_PLANAR;
+  stMWConfig.stVBPoolConfig.astVBPoolSetup[2].u32BlkCount = 1;
+  stMWConfig.stVBPoolConfig.astVBPoolSetup[2].u32Height = 720;
+  stMWConfig.stVBPoolConfig.astVBPoolSetup[2].u32Width = 1280;
+  stMWConfig.stVBPoolConfig.astVBPoolSetup[2].bBind = false;
+
+  // Setup VPSS Grp0
+  stMWConfig.stVPSSPoolConfig.u32VpssGrpCount = 1;
+#ifndef CV186X
+  stMWConfig.stVPSSPoolConfig.stVpssMode.aenInput[0] = VPSS_INPUT_MEM;
+  stMWConfig.stVPSSPoolConfig.stVpssMode.enMode = VPSS_MODE_DUAL;
+  stMWConfig.stVPSSPoolConfig.stVpssMode.ViPipe[0] = 0;
+  stMWConfig.stVPSSPoolConfig.stVpssMode.aenInput[1] = VPSS_INPUT_ISP;
+  stMWConfig.stVPSSPoolConfig.stVpssMode.ViPipe[1] = 0;
+#endif
+
+  SAMPLE_TDL_VPSS_CONFIG_S *pstVpssConfig = &stMWConfig.stVPSSPoolConfig.astVpssConfig[0];
+  pstVpssConfig->bBindVI = true;
+
+  // Assign device 1 to VPSS Grp0, because device1 has 3 outputs in dual mode.
+  VPSS_GRP_DEFAULT_HELPER2(&pstVpssConfig->stVpssGrpAttr, stSensorSize.u32Width,
+                           stSensorSize.u32Height, VI_PIXEL_FORMAT, 1);
+  pstVpssConfig->u32ChnCount = 2;
+  pstVpssConfig->u32ChnBindVI = 0;
+  VPSS_CHN_DEFAULT_HELPER(&pstVpssConfig->astVpssChnAttr[0], stVencSize.u32Width,
+                          stVencSize.u32Height, VI_PIXEL_FORMAT, true);
+  VPSS_CHN_DEFAULT_HELPER(&pstVpssConfig->astVpssChnAttr[1], stVencSize.u32Width,
+                          stVencSize.u32Height, VI_PIXEL_FORMAT, true);
+
+  // Get default VENC configurations
+  SAMPLE_TDL_Get_Input_Config(&stMWConfig.stVencConfig.stChnInputCfg);
+  stMWConfig.stVencConfig.u32FrameWidth = stVencSize.u32Width;
+  stMWConfig.stVencConfig.u32FrameHeight = stVencSize.u32Height;
+
+  // Get default RTSP configurations
+  SAMPLE_TDL_Get_RTSP_Config(&stMWConfig.stRTSPConfig.stRTSPConfig);
+
+  SAMPLE_TDL_MW_CONTEXT stMWContext = {0};
+  s32Ret = SAMPLE_TDL_Init_WM(&stMWConfig, &stMWContext);
+  if (s32Ret != CVI_SUCCESS) {
+    printf("init middleware failed! ret=%x\n", s32Ret);
+    return -1;
+  }
+
+  cvitdl_handle_t stTDLHandle = NULL;
+
+  // Create TDL handle and assign VPSS Grp1 Device 0 to TDL SDK
+  GOTO_IF_FAILED(CVI_TDL_CreateHandle2(&stTDLHandle, 1, 0), s32Ret, create_tdl_fail);
+
+  GOTO_IF_FAILED(CVI_TDL_SetVBPool(stTDLHandle, 0, 2), s32Ret, create_service_fail);
+
+  CVI_TDL_SetVpssTimeout(stTDLHandle, 1000);
+
+  cvitdl_service_handle_t stServiceHandle = NULL;
+  GOTO_IF_FAILED(CVI_TDL_Service_CreateHandle(&stServiceHandle, stTDLHandle), s32Ret,
+                 create_service_fail);
+
+  GOTO_IF_FAILED(CVI_TDL_OpenModel(stTDLHandle, CVI_TDL_SUPPORTED_MODEL_SCRFDFACE, argv[1]), s32Ret,
+                 setup_tdl_fail);
+
+  pthread_t stVencThread, stTDLThread;
+  SAMPLE_TDL_VENC_THREAD_ARG_S args = {
+      .pstMWContext = &stMWContext,
+      .stServiceHandle = stServiceHandle,
+  };
+
+  pthread_create(&stVencThread, NULL, run_venc, &args);
+  pthread_create(&stTDLThread, NULL, run_tdl_thread, stTDLHandle);
+
+  pthread_join(stVencThread, NULL);
+  pthread_join(stTDLThread, NULL);
+
+setup_tdl_fail:
+  CVI_TDL_Service_DestroyHandle(stServiceHandle);
+create_service_fail:
+  CVI_TDL_DestroyHandle(stTDLHandle);
+create_tdl_fail:
+  SAMPLE_TDL_Destroy_MW(&stMWContext);
+
+  return 0;
+}
